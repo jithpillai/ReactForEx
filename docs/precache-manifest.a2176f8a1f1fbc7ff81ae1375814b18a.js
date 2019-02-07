adcf8e99d3d9785d0902a3fca745fd7bff471b7a
Jith Pillai <prijithkr@gmail.com>
2019-02-07 22:18:15 +0530

    Added Docs folder for Demo

---
+++ docs/precache-manifest.a2176f8a1f1fbc7ff81ae1375814b18a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8128cdb2db173bfae1f6",
+    "url": "/static/js/main.8128cdb2.chunk.js"
+  },
+  {
+    "revision": "b8a54d6678923d5c47ed",
+    "url": "/static/js/1.b8a54d66.chunk.js"
+  },
+  {
+    "revision": "8128cdb2db173bfae1f6",
+    "url": "/static/css/main.eaad2c9d.chunk.css"
+  },
+  {
+    "revision": "b8a54d6678923d5c47ed",
+    "url": "/static/css/1.da68a3b5.chunk.css"
+  },
+  {
+    "revision": "1ca2c38369ffa2b0341f08f1a1d83b19",
+    "url": "/index.html"
+  }
+];
